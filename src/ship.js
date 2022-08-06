565a16a433b6b9aa3c6b0c6f425c275e70f9b4d3
kmakai <makaikwag@gmail.com>
2022-08-06 16:36:38 -0400

    4 files modified

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,14 @@
-class Ship {
-  constructor(){
-    
+export default class Ship {
+  constructor(length) {
+    this.length = length;
+  }
+
+  hit() {
+    this.length--;
+  }
+
+  isSunk() {
+    if (!this.length) true;
+    else false;
   }
 }
