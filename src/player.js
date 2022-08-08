59a7ef6d4c05ee3fd33d7b06dc79b794eca871ab
kmakai <makaikwag@gmail.com>
2022-08-08 18:46:55 -0400

    2 files modified

--- src/player.js
+++ src/player.js
@@ -1,18 +1,23 @@
 class Player {
-  constructor(name){
+  constructor(name) {
     this.name = name;
-    this.board; 
+    this.board;
   }
 }
 
 class Ai extends Player {
-  constructor(name){
+  constructor(name) {
     super(name);
-    movesMade = [];
   }
 
-  takeShot(board){
-    
-  }
+  takeShot(board) {
+    let [x, y] = [
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+    ];
 
+    if(board[x][y] === "miss" || board[x[y]] === "hit") this.takeShot();
+
+    board.receiveAttack([x, y]);
+  }
 }
