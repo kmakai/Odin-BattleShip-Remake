0413d4c0b22f81b3a7df6e83c41a6e738e53a084
kmakai <makaikwag@gmail.com>
2022-08-10 18:38:14 -0400

    3 files modified

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-const GameBoard = require("./gameboard");
-const Ship = require("./ship");
-const { Player, Ai } = require("./player");
+const GameBoard = require('./gameboard');
+const Ship = require('./ship');
+const { Player, Ai } = require('./player');
 
 // Get Dom elements.
-const PlayerOneName = document.querySelector(".player1 .name");
-const PlayerOneMsg = document.querySelector(".player1 .msg");
-const PlayerOneBoard = document.querySelector(".player1 .board");
+const PlayerOneName = document.querySelector('.player1 .name');
+const PlayerOneMsg = document.querySelector('.player1 .msg');
+const PlayerOneBoard = document.querySelector('.player1 .board');
 
-const PlayerTwoName = document.querySelector(".player2 .name");
-const PlayerTwoMsg = document.querySelector(".player2 .msg");
-const PlayerTwoBoard = document.querySelector(".player2 .board");
+const PlayerTwoName = document.querySelector('.player2 .name');
+const PlayerTwoMsg = document.querySelector('.player2 .msg');
+const PlayerTwoBoard = document.querySelector('.player2 .board');
 
 // Global Variables.
 let currentPlayer;
 
 // Create Player and Ai.
-const playerOne = new Player("Captain");
+const playerOne = new Player('Captain');
 PlayerOneName.textContent = playerOne.name;
 
-const playerTwo = new Ai("Bot");
+const playerTwo = new Ai('Bot');
 PlayerTwoName.textContent = playerTwo.name;
 
 // Create Boards.
@@ -26,58 +26,66 @@ playerOne.board = new GameBoard();
 playerTwo.board = new GameBoard();
 
 // Create ships and place them.
-playerOne.board.placeShip(new Ship(5), [0, 0], "h");
-playerOne.board.placeShip(new Ship(4), [2, 0], "v");
-playerOne.board.placeShip(new Ship(3), [0, 9], "v");
-playerOne.board.placeShip(new Ship(3), [5, 5], "h");
-playerOne.board.placeShip(new Ship(2), [8, 5], "v");
+playerOne.board.placeShip(new Ship(5), [0, 0], 'h');
+playerOne.board.placeShip(new Ship(4), [2, 0], 'v');
+playerOne.board.placeShip(new Ship(3), [0, 9], 'v');
+playerOne.board.placeShip(new Ship(3), [5, 5], 'h');
+playerOne.board.placeShip(new Ship(2), [8, 5], 'v');
 
-playerTwo.board.placeShip(new Ship(5), [3, 1], "h");
-playerTwo.board.placeShip(new Ship(4), [2, 0], "v");
-playerTwo.board.placeShip(new Ship(3), [0, 9], "v");
-playerTwo.board.placeShip(new Ship(3), [7, 4], "h");
-playerTwo.board.placeShip(new Ship(2), [7, 2], "v");
+playerTwo.board.placeShip(new Ship(5), [3, 1], 'h');
+playerTwo.board.placeShip(new Ship(4), [2, 0], 'v');
+playerTwo.board.placeShip(new Ship(3), [0, 9], 'v');
+playerTwo.board.placeShip(new Ship(3), [7, 4], 'h');
+playerTwo.board.placeShip(new Ship(2), [7, 2], 'v');
 
 // Display the boards
 const displayBoards = function () {
   let board = playerOne.board.board;
   board.forEach((row, rI) => {
     row.forEach((cell, cI) => {
-      let id = typeof cell === "object" ? "ship" : cell === "hit" ? "hit" : "e";
-      const content = cell === "hit" || cell === "miss" ? cell : "";
+      let id =
+        typeof cell === 'object'
+          ? 'ship'
+          : cell === 'hit'
+          ? 'hit'
+          : cell === 'miss'
+          ? 'miss'
+          : 'e';
+      const content = cell === 'hit' || cell === 'miss' ? cell : '';
       const html = `  <div class="cell" id="${id}" data-x="${rI}" data-y="${cI}">${
-        typeof cell === "object" ? "ship" : content
+        typeof cell === 'object' ? '' : content
       }</div>
       `;
 
-      PlayerOneBoard.insertAdjacentHTML("beforeend", html);
+      PlayerOneBoard.insertAdjacentHTML('beforeend', html);
     });
   });
 
   board = playerTwo.board.board;
   board.forEach((row, rI) => {
     row.forEach((cell, cI) => {
-      const content = cell === "hit" || cell === "miss" ? cell : "";
-      const html = `  <div class="cell" data-x="${rI}" data-y="${cI}">${content}</div>
+      let id = cell === 'hit' ? 'hit' : cell === 'miss' ? 'miss' : 'e';
+      const content = cell === 'hit' || cell === 'miss' ? cell : '';
+      const html = `  <div class="cell" id="${id}" data-x="${rI}" data-y="${cI}">${content}</div>
       `;
 
-      PlayerTwoBoard.insertAdjacentHTML("beforeend", html);
+      PlayerTwoBoard.insertAdjacentHTML('beforeend', html);
     });
   });
 };
 
 const updateBoard = function () {
-  PlayerOneBoard.innerHTML = "";
-  PlayerTwoBoard.innerHTML = "";
+  PlayerOneBoard.innerHTML = '';
+  PlayerTwoBoard.innerHTML = '';
   displayBoards();
 };
 
 displayBoards();
 
 // PLayer moves.
-PlayerTwoBoard.addEventListener("click", (e) => {
+PlayerTwoBoard.addEventListener('click', (e) => {
   if (currentPlayer === playerTwo) return;
-  if (e.target.textContent !== "") return;
+  if (e.target.textContent !== '') return;
   const [x, y] = [e.target.dataset.x, e.target.dataset.y];
   const board = playerTwo.board;
   board.receiveAttack([x, y]);
